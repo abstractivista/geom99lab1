f448f1e549c4cf8683572bdacb5ce5a307f3c451
abstractivista <91274183+abstractivista@users.noreply.github.com>
2023-01-25 22:54:00 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -9,44 +9,32 @@ function initMap() {
     center: kauai,
     mapTypeId: 'satellite'
   });
+  
+// Initialize and add the map
+function initMap() {
+  // The locations in Kauai
+  var kauai = { lat: 22.058, lng: -159.579 };
+  var hanalei = { lat: 22.1999637, lng: -159.4694653 };
+  var poipu = { lat: 21.8731483, lng: -159.455060}
+  var kilauea = { lat: 22.2316946, lng: -159.4019598}
+  var waialeale = { lat: 22.0706509, lng: -159.5136368}
+  // The map, centered at Kauai
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 9,
+    center: kauai,
+    mapTypeId: 'satellite'
+  });
+  // The marker, positioned at Kauai
+  const marker = new google.maps.Marker({
+    position: kauai,
+    map: map,
   // Marker positions on Kauai
-var locations = [
+  var locations = [
   [ 'Hanalei Wildlife Refuge', 22.1999637,-159.4694653 ]
    ,[ 'Poipu Beach', 21.8731483,-159.455060 ]
   ,[ 'Kilauea Lighthouse',22.2316946,-159.4019598 ]
   , [ 'Waialeale', 22.0706509,-159.5136368 ]
   ];
-
-for ( var i = 0; i < locations.length; i++ ) 
-			{
-				// get current location
-				var location = locations[ i ];
-				
-				// create map position
-				var position = new google.maps.LatLng( location[ 1 ], location[ 2 ] );
-				
-				// add position to bounds
-				bounds.extend( position );
-				
-				// create marker (https://developers.google.com/maps/documentation/javascript/reference#MarkerOptions)
-				var marker = new google.maps.Marker({
-					animation: google.maps.Animation.DROP
-					, icon: "http://www.google.com/intl/en_us/mapfiles/ms/micons/blue-dot.png"
-					, map: map
-					, position: position
-					, title: location[ 0 ]
-				});
-				
-				// create info window and add to marker (https://developers.google.com/maps/documentation/javascript/reference#InfoWindowOptions)
-				google.maps.event.addListener( marker, 'click', ( 
-					function( marker, i ) {
-						return function() {
-							var infowindow = new google.maps.InfoWindow();
-							infowindow.setContent( locations[ i ][ 0 ] );
-							infowindow.open( map, marker );
-						}
-					}
-				)( marker, i ) );
-			}
-
+  });
+}
 window.initMap = initMap;
