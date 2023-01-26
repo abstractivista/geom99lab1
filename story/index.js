6afcf693a4e5ee0fe1085b4b8a5898a802bb327e
abstractivista <91274183+abstractivista@users.noreply.github.com>
2023-01-25 20:47:54 -0500

    Create index.js

---
+++ story/index.js
@@ -0,0 +1,17 @@
+// Initialize and add the map
+function initMap() {
+  // The location of Kauai
+  const kauai = { lat: -22.058, lng: -159.579 };
+  // The map, centered at Kauai
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: Kauai,
+  });
+  // The marker, positioned at Kauai
+  const marker = new google.maps.Marker({
+    position: Kauai,
+    map: map,
+  });
+}
+
+window.initMap = initMap;
