39d868cba8a45c700a7744854678187e48b5b390
abstractivista <91274183+abstractivista@users.noreply.github.com>
2023-01-25 22:18:55 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -1,16 +1,21 @@
 // Initialize and add the map
 function initMap() {
-  // The location of Kauai
-  const kauai = { lat: 22.058, lng: -159.579 };
+  // Initial location for Kauai and locations of sites visited
+  const kauai = { lat: 22.058, lng: -159.579 }
+  const hanalei = { lat: 22.199, lng: -159.469 }
+;
   // The map, centered at Kauai
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 9,
     center: kauai,
     mapTypeId: 'satellite'
   });
-  // The marker, positioned at Kauai
-  const marker = new google.maps.Marker({
+  // Marker positions on Kauai
+  const marker1 = new google.maps.Marker({
     position: kauai,
+    map: map,
+  const marker2 = new google.maps.Marker({
+    position: hanalei,
     map: map,
   });
 }
